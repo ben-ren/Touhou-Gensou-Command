cac6ca8c5381a00a4487d171deaa38e47d2465f6
Benjamin Rendell <benjamin.rendell@students.mq.edu.au>
2025-11-01 12:21:14 +1100

    Focus mode, primary & secondary fire. Dynamic firing speeds.

--- Assets/Scripts/ProjectileSpawner.cs
+++ Assets/Scripts/ProjectileSpawner.cs
@@ -6,9 +6,18 @@ public class ProjectileSpawner : MonoBehaviour
     [Header("Input Settings")]
     public InputActionAsset inputActionAsset;
     private InputAction _fire;
+    private InputAction _brake;
 
     [Header("Projectile")]
-    public GameObject prefab;
+    public GameObject primaryPrefab;
+    public GameObject secondaryPrefab;
+    private GameObject projectile;
+
+    [Header("Firing Settings")]
+    [SerializeField] private float primaryFireRate = 1f; // shots per second for primary fire
+    [SerializeField] private float secondaryFireRate = 5f; // shots per second for secondary fire (focus mode)
+    private float fireRate = 5f; // shots per second
+    private float nextFireTime = 0f;              // internal timer
 
     [Header("References")]
     private GameObject parent;
@@ -18,14 +27,25 @@ public class ProjectileSpawner : MonoBehaviour
     {
         _fire = inputActionAsset.FindAction("Player/PrimaryFire");
         _fire?.Enable();
+        _brake = inputActionAsset.FindAction("Brake");
+
         parent = gameObject.transform.parent.gameObject;
+        projectile = primaryPrefab; // default mode
     }
 
     // Update is called once per frame
     void Update()
     {
+        SwitchFiringModes();
         RepositionSpawner();
         FireProjectile();
+    }
+
+    void SwitchFiringModes()
+    {
+        float brakeValue = _brake.ReadValue<float>();
+        projectile = brakeValue > 0 ? secondaryPrefab : primaryPrefab;
+        fireRate = brakeValue > 0 ? secondaryFireRate : primaryFireRate;
     }
 
     //Move into position between crosshair & camera
@@ -38,8 +58,11 @@ public class ProjectileSpawner : MonoBehaviour
     //BUG: Doesn't fire projectile in direction of ProjectileSpawner. (Instantiate in same rotational direction as ProjectileSpawner)
     void FireProjectile()
     {
-        if (_fire.WasPerformedThisFrame()){
-            Instantiate(prefab, transform.position, transform.rotation); //(prefab object, new postion, new rotation, parent object, WorldSpace?:boolean)   
+        float fireInput = _fire.ReadValue<float>();
+
+        if (fireInput > 0 && Time.time >= nextFireTime){
+            nextFireTime = Time.time + (1f / fireRate); // set next available fire time
+            Instantiate(projectile, transform.position, transform.rotation); //(prefab object, new postion, new rotation, parent object, WorldSpace?:boolean)   
         }
     }
 }
